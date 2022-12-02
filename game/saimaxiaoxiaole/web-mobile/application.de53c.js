f62b8c9da8d37de666e9f039dbccbf4f57c00a85
qiaopc <908694753@qq.com>
2022-12-02 18:35:57 +0800

    update

--- game/saimaxiaoxiaole/web-mobile/application.de53c.js
+++ game/saimaxiaoxiaole/web-mobile/application.de53c.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.json';
+          this.settingsPath = 'src/settings.854a0.json';
           this.showFPS = false;
         }
 
--- game/saimaxiaoxiaole/web-mobile/index.f690a.js
+++ game/saimaxiaoxiaole/web-mobile/index.f690a.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.de53c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
+    setters: [function (_applicationDe53cJs) {
+      Application = _applicationDe53cJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
